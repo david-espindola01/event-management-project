625be8a467d581404f1192e719b7aea2611ca75a
david-espindola01 <david.espindola01@uptc.edu.co>
2026-02-24 14:56:04 -0500

    Delete Front-Admin directory

--- Front-Admin/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/Stefanny%20Arias/Desktop/OCTAVO%20SEMESTRE/INGENIER%C3%8DA%20DE%20SOFTWARE%20II/nuxt-admin%20-%20copia%20-%20copia/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
